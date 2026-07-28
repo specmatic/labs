fb056ed2ef64a2ee7971460a2674569af9bab75f
Joel Rosario <cirquitz@gmail.com>
2026-07-28 13:24:56 +0530

    fix(api-security): load client keystore credentials from Vault

--- api-security-schemes/vault/import-oauth-secrets.sh
+++ api-security-schemes/vault/import-oauth-secrets.sh
@@ -10,10 +10,14 @@ KEYCLOAK_USER_USERNAME="$(jq -er '.data.data.KEYCLOAK_USER_USERNAME' <<< "${vaul
 KEYCLOAK_USER_PASSWORD="$(jq -er '.data.data.KEYCLOAK_USER_PASSWORD' <<< "${vault_response}")" || return 1
 KEYCLOAK_SERVICE_ACCOUNT_USERNAME="$(jq -er '.data.data.KEYCLOAK_SERVICE_ACCOUNT_USERNAME' <<< "${vault_response}")" || return 1
 KEYCLOAK_SERVICE_ACCOUNT_PASSWORD="$(jq -er '.data.data.KEYCLOAK_SERVICE_ACCOUNT_PASSWORD' <<< "${vault_response}")" || return 1
+SPECMATIC_CLIENT_KEYSTORE_PASSWORD="$(jq -er '.data.data.SPECMATIC_CLIENT_KEYSTORE_PASSWORD' <<< "${vault_response}")" || return 1
+SPECMATIC_CLIENT_KEY_PASSWORD="$(jq -er '.data.data.SPECMATIC_CLIENT_KEY_PASSWORD' <<< "${vault_response}")" || return 1
 
 export KEYCLOAK_USER_USERNAME
 export KEYCLOAK_USER_PASSWORD
 export KEYCLOAK_SERVICE_ACCOUNT_USERNAME
 export KEYCLOAK_SERVICE_ACCOUNT_PASSWORD
+export SPECMATIC_CLIENT_KEYSTORE_PASSWORD
+export SPECMATIC_CLIENT_KEY_PASSWORD
 
 unset vault_response vault_token
